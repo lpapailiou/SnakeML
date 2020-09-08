65fae1386b8945738ee3b819f52e9dc90a42fd0b
Sandro Bürki <sandro.buerki@bbcag.ch>
2020-09-08 22:37:50 +0200

    project structure and initial snake prototype

---
+++ src/main/java/game/Config.java
@@ -0,0 +1,29 @@
+package game;
+
+import game.Cell;
+import game.Direction;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Config {
+
+  public static final double GAME_OVER_FONT_SIZE = 100;
+  public static final Paint GAME_OVER_FONT_COLOR = Color.RED;
+
+  public static final Paint SCORE_FONT_COLOR = Color.WHITE;
+  public static final double SCORE_FONT_SIZE_IN_PT = 30;
+
+  public static final Color BACKGROUND_COLOR = Color.BURLYWOOD;
+  public static final Paint SNAKE_BODY_COLOR = Color.SIENNA;
+
+  public static final int CELL_SIZE_IN_PX = 100;
+  public static final int NUMBER_OF_CELL_COLUMNS = 20;
+  public static final int NUMBER_OF_CELL_ROWS = 20;
+
+  public static final int INITIAL_SNAKE_SIZE = 3;
+  public static final Direction INITIAL_DIRECTION = Direction.RIGHT;
+  public static final Cell INITIAL_STARTING_POINT = new Cell(3, 3);
+
+  public static final int SPEED_FACTOR = 7;
+
+}
---
+++ src/main/java/game/Snake.java
@@ -0,0 +1,51 @@
+package game;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Snake {
+
+  static LinkedList<Cell> body = new LinkedList<>();
+
+  public Snake(int size, Direction initialDirection, Cell initialHeadPosition) {
+
+    andGodSaidLetThereBeSnake(size, initialDirection, initialHeadPosition);
+  }
+
+  private void andGodSaidLetThereBeSnake(int size, Direction initialDirection,
+      Cell initialHeadPosition) {
+    for (int i = 0; i < size; i++) {
+      body.add(new Cell(initialHeadPosition.x - i * initialDirection.x,
+          initialHeadPosition.y - i * initialDirection.y));
+    }
+  }
+
+  public void move(Direction direction) {
+    body.removeLast();
+
+    Cell snakeHead = body.getFirst();
+    body.addFirst(new Cell(snakeHead.x + direction.x, snakeHead.y + direction.y));
+  }
+
+  public boolean isDead() {
+    return isSnakeInWall() || isSnakeInItself();
+  }
+
+  private boolean isSnakeInItself() {
+    return body.stream().distinct().count() < (long) body.size();
+  }
+
+  private boolean isSnakeInWall() {
+    Cell snakeHead = body.get(0);
+    if (snakeHead.x > Config.NUMBER_OF_CELL_COLUMNS || snakeHead.x < 0) {
+      return true;
+    }
+
+    return snakeHead.y > Config.NUMBER_OF_CELL_ROWS || snakeHead.y < 0;
+  }
+
+
+  public List<Cell> getBody() {
+    return body;
+  }
+}
