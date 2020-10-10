f02058aee9371963ce86c103191470e59e74d40b
lpapailiou <lena@kaiki.ch>
2020-10-10 15:22:58 +0200

    fitness function changed

--- src/test/java/ai/GameBatchTest.java
+++ src/test/java/ai/GameBatchTest.java
@@ -13,10 +13,23 @@ public class GameBatchTest {
     int generations = config.getGenerationCount();
     int population = config.getPopulationSize();
 
-    config.setGenerationCount(400);
+    config.setGenerationCount(1000);
     config.setPopulationSize(800);
-    GameBatch batch = new GameBatch(new NeuralNetwork(0.999,12,32,32,32,4));
+    GameBatch batch = new GameBatch(new NeuralNetwork(0.6,12,32,32,4));
     batch.run();
+
+    /*
+    generations: 1000
+    population : 800
+    net: new GameBatch(new NeuralNetwork(0.6,12,32,32,4));
+    max fitness for gen #0: 	314 	(snake length: 3)
+    max fitness for gen #21: 	374232 	(snake length: 32)
+    max fitness for gen #213: 	6719464 	(snake length: 70)
+    max fitness for gen #231: 	17729424 	(snake length: 91)
+    ----------------------------------------------> perfect game!!!
+    max fitness for gen #243: 	813761003 	(snake length: 256)
+    from gen #310: perfect games only
+     */
 
     config.setGenerationCount(generations);
     config.setPopulationSize(population);
