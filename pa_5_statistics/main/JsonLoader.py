3b9bf77070bb926124c1ad0740ea899189852d0f
ramona.koksa@students.ffhs.ch <ramona.koksa@students.ffhs.ch>
2020-10-11 14:35:55 +0200

    implemented json loader

--- pa_5_statistics/main/JsonLoader.py
+++ pa_5_statistics/main/JsonLoader.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import tempfile
+import json
 import os
+
 
 class JsonLoader(object):
     """
@@ -7,12 +10,22 @@ class JsonLoader(object):
     """
 
     def __init__(self):
-        self.__path_to_json = "C:\\$SB52EF.tmpUsers\\{}\\AppData\\Local\\Temp".format(os.getlogin())
-
+        self.__path_to_json = tempfile.gettempdir()
 
     def read_json(self):
         """
         This method reads the generated Json-File from USER\AppData\Local\Temp
         :return: json
         """
+        dir = str(self.__path_to_json)
 
+        prefix = [i for i in os.listdir(self.__path_to_json) if os.path.isfile(os.path.join(self.__path_to_json, i)) and
+                  'SnakeML_batchData_' in i]
+
+        listToString = ''.join([str(file) for file in prefix])
+
+        file = dir + "\\" + listToString
+
+        with open(file) as file:
+            df = json.load(file)
+            return df
