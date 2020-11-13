5f3a049b5f0e49f64a799ba314722a9fdd61c362
lpapailiou <lena@kaiki.ch>
2020-11-13 22:04:41 +0100

    merged both feature branches

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,7 +18,38 @@ import java.util.concurrent.CountDownLatch;
 public class Main extends Application {
 
   public static void main(String[] args) {
+    launch(args);
+  }
 
+  @Override
+  public void start(Stage stage) {
+
+    IMainConfigReader config = Config.getMainConfigReader();
+
+    try {
+      ClassLoader classLoader = Main.class.getClassLoader();
+      FXMLLoader loader = new FXMLLoader(classLoader.getResource("ApplicationPanel.fxml"));
+      Parent root = loader.load();
+      Scene scene = new Scene(root, 1600, 800);
+      scene.getStylesheets().add(classLoader.getResource("applicationCss.css").toExternalForm());
+      scene.getStylesheets().add(Main.class.getClassLoader().getResource(config.getTheme().getCss()).toExternalForm());
+      scene.setFill(config.getTheme().getBackgroundColor());
+      stage.setScene(scene);
+      stage.setMinHeight(839);
+      stage.setMinWidth(1616);
+      stage.setMaxHeight(839);
+      stage.setMaxWidth(1616);
+      stage.setTitle("Snake ML | FFHS Bern 2020");
+      stage.getIcons().add(new Image("snake.png"));
+      ((ApplicationController) loader.getController()).setStage(stage);
+      stage.show();
+      startWebServer();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void startWebServer() {   // TODO: slows down startup of gui
     Service<Void> service = new Service<Void>() {
       @Override
       protected Task<Void> createTask() {
@@ -44,36 +75,6 @@ public class Main extends Application {
       }
     };
     service.start();
-
-    launch(args);
-
-  }
-
-  @Override
-  public void start(Stage stage) {
-
-    IMainConfigReader config = Config.getMainConfigReader();
-
-    try {
-      ClassLoader classLoader = Main.class.getClassLoader();
-      FXMLLoader loader = new FXMLLoader(classLoader.getResource("ApplicationPanel.fxml"));
-      Parent root = loader.load();
-      Scene scene = new Scene(root, 1600, 800);
-      scene.getStylesheets().add(classLoader.getResource("applicationCss.css").toExternalForm());
-      scene.getStylesheets().add(Main.class.getClassLoader().getResource(config.getTheme().getCss()).toExternalForm());
-      scene.setFill(config.getTheme().getBackgroundColor());
-      stage.setScene(scene);
-      stage.setMinHeight(839);
-      stage.setMinWidth(1616);
-      stage.setMaxHeight(839);
-      stage.setMaxWidth(1616);
-      stage.setTitle("Snake ML | FFHS Bern 2020");
-      stage.getIcons().add(new Image("snake.png"));
-      ((ApplicationController) loader.getController()).setStage(stage);
-      stage.show();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
 }
